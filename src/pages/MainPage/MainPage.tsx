0c505fd0e1de3c86d6dde28d08bfd076ac8a235b
Avilov01 <Avilov01@gmail.com>
2023-03-10 18:12:41 +0300

    fix has scrolled on NewsPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,6 +1,7 @@
 import { Grid } from "@mui/material";
 
 import { LoadIndicator, NewsCardRow } from "../../components";
+import { ContentWrapper } from "../../styled";
 import { News } from "../../types";
 
 type MainPageProps = {
@@ -13,10 +14,12 @@ export const MainPage = ({ news }: MainPageProps) => {
   }
 
   return (
-    <Grid container spacing={1}>
-      {news.map((item) => {
-        return <NewsCardRow key={item.id} item={item} />;
-      })}
-    </Grid>
+    <ContentWrapper>
+      <Grid container spacing={1}>
+        {news.map((item) => {
+          return <NewsCardRow key={item.id} item={item} />;
+        })}
+      </Grid>
+    </ContentWrapper>
   );
 };
