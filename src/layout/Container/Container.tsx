d4fdfdb77ff34235bf91f10ce850a8e3d8ecbb30
Avilov01 <Avilov01@gmail.com>
2023-03-10 17:47:57 +0300

    add layout

---
+++ src/layout/Container/Container.tsx
@@ -0,0 +1,13 @@
+import { Box, styled } from "@mui/material";
+
+export const Container = styled(Box)({
+  display: "flex",
+  justifyContent: "flex-start",
+  alignItems: "flex-start",
+  flexDirection: "column",
+  backgroundColor: "#f6f6f6",
+  height: "calc(100% - 40px)",
+  width: "1080px",
+  borderRadius: 25,
+  padding: "20px 30px",
+});
